a328f4d8558fe7f7a2f4da445f6f1c366f292fc5
regulyator <regulyator777@gmail.com>
2021-12-12 01:17:52 +0300

    init

---
+++ src/main/java/com/regulyator/santabot/SantaBotApplication.java
@@ -0,0 +1,13 @@
+package com.regulyator.santabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regulyator/santabot/SantaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regulyator.santabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
